d0eb665379a4ca4ab45c848cb0ad2d4be4dadb4f
Kirill <some@gmail.com>
2021-12-26 15:33:01 +0300

    Модалка с отправлением данных

--- src/modules/ActivityContainer/ActivityContainer.tsx
+++ src/modules/ActivityContainer/ActivityContainer.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import { WishCard } from './components/WishCard';
+import { WishCard } from "./components/WishCard";
 
-import wishTest from '../../assets/wishTest.svg';
+import wishTest from "../../assets/wishTest.svg";
 
-import './container.scss';
+import "./container.scss";
+import { Modal } from "../../components/Modal/Modal";
+import { useState } from "react";
 
-export const ActivityContainer = function() {
-  return(
-    <div className='activity-container'>
-      <div className='card-container'>
-        <WishCard costLevel={1} name="default image"/>
-        <WishCard imgSrc={wishTest} costLevel={3} name="with image and loooooooooooong text"/>
+export const ActivityContainer = function () {
+  const [openModal, setOpenModal] = useState(false);
+
+  const handlerOpenModal = () => {
+    setOpenModal(!openModal);
+  };
+
+  const handlerClose = () => {
+    setOpenModal(false);
+  };
+
+  return (
+    <div className="activity-container">
+      <div className="card-container">
+        <WishCard costLevel={1} name="default image" />
+        <WishCard
+          imgSrc={wishTest}
+          costLevel={3}
+          name="with image and loooooooooooong text"
+        />
+        <button onClick={handlerOpenModal}>click here</button>
+        {openModal && <Modal handlerClose={handlerClose} />}
       </div>
-
     </div>
   );
 };
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,8 +1,7 @@
-import { AnyAction, ThunkDispatch } from "@reduxjs/toolkit"
+import { AnyAction, ThunkDispatch } from "@reduxjs/toolkit";
 
 export type CommonResponse = {
-  error?: string;
-  [key: string]: string | number | undefined;
-}
+  [key: string]: string | number | undefined | any;
+};
 
 export type AnyThunkDispatch = ThunkDispatch<any, any, AnyAction>;
